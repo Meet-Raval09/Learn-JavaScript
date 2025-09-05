8eb2716a333ba00d4336eda78236debbf1158241
Meet P Raval <iameetraval@gmail.com>
2025-09-05 09:17:36 +0530

    Added Math and Numbers File

---
+++ Number_And_Math.js
@@ -0,0 +1,26 @@
+// Number And Math In JavaScript.
+
+// Numbers In JavaScript.
+const score = 180;
+const newnum = new Number(120);
+//console.log(score);
+//console.log(newnum);
+//console.log(newnum.toString.length);
+//console.log(newnum.toFixed(2));
+//const other_no = 123.456123445;
+//console.log(other_no.toPrecision(5))
+const hundreads = 1000000000;
+//console.log(hundreads.toLocaleString("en-IN"));
+
+// Mathe In JavaScript.
+//console.log(Math)
+// Abs = convert negative value into a positive value.
+//console.log(Math.abs(-4));
+//console.log(Math.round(123.60));
+//console.log(Math.ceil(5.2))
+//console.log(Math.floor(3.6))
+
+//console.log((Math.random()*10)+1);
+const max_val = 20;
+const min_val = 10;
+console.log(Math.floor(Math.random() * (max_val - min_val + 1) + min_val))
