6200132175c5e9f88f2a2019c1453ddaa76914aa
Meet P Raval <iameetraval@gmail.com>
2025-11-06 10:02:08 +0530

    Some Changes

--- JS_Projects/JS/Color_Changer_Switcher.js
+++ JS_Projects/JS/Color_Changer_Switcher.js
@@ -6,19 +6,19 @@ let msgbox = document.querySelector(".msg-box");
 button.forEach(function(btn){
  btn.addEventListener("click",function(e){
    if(e.target.id == "red-box"){
-     body.style.backgroundColor = "red";
+     body.style.backgroundColor = e.target.id;
      msgbox.innerHTML = "Background Color Changed to Red";
    }
    else if(e.target.id == "blue-box"){
-     body.style.backgroundColor = "blue";
+     body.style.backgroundColor = e.target.id;
      msgbox.innerHTML = "Background Color Changed to Blue";
    }
    else if(e.target.id == "green-box"){
-     body.style.backgroundColor = "green";
+     body.style.backgroundColor = e.target.id;
      msgbox.innerHTML = "Background Color Changed to Green";
    }
    else if(e.target.id == "yellow-box"){
-     body.style.backgroundColor = "yellow";
+     body.style.backgroundColor = e.target.id;
      msgbox.innerHTML = "Background Color Changed to Yellow";
    }
  })
